5d8a9088076943e55eeb191cbc75312bfdc5fa87
e2295368 <100291026+e2295368@users.noreply.github.com>
2022-03-19 01:04:21 -0400

    Add files via upload

---
+++ Afshin_Labo1.py/problem5.py
@@ -0,0 +1,15 @@
+
+from decimal import Decimal, Rounded
+from this import d
+from unicodedata import decimal
+
+
+def multiplier_et_diviser( p1,p2,p3):
+    return((p1*p2)/p3)
+
+p1=round(7.251)
+p2=round(6.645)
+p3=round(10.257)
+
+print(multiplier_et_diviser(p1,p2,p3))
+ 
