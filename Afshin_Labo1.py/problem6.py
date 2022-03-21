40e67106e4c76bfca6dffc73acf0859f428f99b2
Afshin <100291026+e2295368@users.noreply.github.com>
2022-03-21 10:05:46 -0400

    Delete problem6.py

--- Afshin_Labo1.py/problem6.py
+++
@@ -1,6 +0,0 @@
-repas = str(input("entrez votre repas prefere : "))
-pays = str(input("entrez votre pays prefere : "))
-anne = str(input("entrez votre l'année future  : "))
-
-print(" Vous aurez l'opportunité de manger " , repas , "lorsque vous visiterai " , pays ,  "en" , anne ) 
-
