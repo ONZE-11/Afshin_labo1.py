e03199c157a16859540c3b6904ea10e84cdf2604
Afshin <100291026+e2295368@users.noreply.github.com>
2022-03-21 09:06:38 -0400

    Update problem1.py

--- Afshin_Labo1.py/problem1.py
+++ Afshin_Labo1.py/problem1.py
@@ -2,8 +2,7 @@ def choisissez_nom_ou_nombre():
     print("#1. Afshin: ")
     print("#2. Nombre: ")
 
-option=int(input("Entrez nom(1) ou nombre(2): "))
-
+option=int(input("Entrez nom(1) ou nombre(2) : "))
 if option==1:
     print("Afshin")
 
@@ -12,4 +11,69 @@ elif(option==2):
 
 else:
     print(" votre choix n est pas valid ! ")
+#problem2
+def faire_puissance (nombre1,nombre2):
+    return(nombre1**nombre2)
+
+print("#1. entrez nombre1: ")
+print("#2. entrez nombre2: ")
+
+option1=int(input("Entrez nom(1): "))
+option2=int(input("Entrez nom(2): "))
+
+nombre1=option1
+nombre2=option2
+
+print(faire_puissance(nombre1,nombre2))
+#problem3
+def diviser_par_3_ou_3():
+  
+    Nombre=int(input("Entrez un nombre : "))
+
+
+    if(Nombre%2==0):
+        print("ce nombre est divisible par 2 :" ,Nombre )
+
+    elif(Nombre%3==0):
+        print("ce nombre est divisible par 3 :" ,Nombre )   
+
+    else:
+        print(Nombre)
+
+
+diviser_par_3_ou_3()
+
+#problem 4
+from re import M
+
+
+def date_de_naissance(Nombre,m):
+    return(Nombre+m)
+
+m=50    
+Nombre=int(input("Entrez votre année de naissance : "))
+
+
+print("vous etes 50 ans dans cette anne:" , date_de_naissance(Nombre,m))
+
+#problem5
+from unicodedata import decimal
+
+
+def multiplier_et_diviser( p1,p2,p3):
+    return((p1*p2)/p3)
+
+p1=decimal(7.250)
+p2=decimal(10.725)
+p3=decimal(2.256)
+
+print(multiplier_et_diviser(p1,p2,p3)) 
+
+#problem6
+repas = str(input("entrez votre repas prefere : "))
+pays = str(input("entrez votre pays prefere : "))
+anne = str(input("entrez votre l'année future  : "))
+
+print(" Vous aurez l'opportunité de manger " , repas , "lorsque vous visiterai " , pays ,  "en" , anne ) 
+
 
