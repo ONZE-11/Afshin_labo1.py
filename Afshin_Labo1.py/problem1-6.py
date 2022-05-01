b92e240b4d5c7bea339b800a688420b38f7193f4
Afshin <100291026+e2295368@users.noreply.github.com>
2022-05-01 09:45:56 -0400

    Update problem1-6.py

--- Afshin_Labo1.py/problem1-6.py
+++ Afshin_Labo1.py/problem1-6.py
@@ -1,100 +1,122 @@
-def choisissez_nom_ou_nombre():
-    print("#1. Afshin: ")
-    print("#2. Nombre: ")
+sortie = False
+while(not sortie):
 
-option=int(input("Entrez nom(1) ou nombre(2) : "))
-if option==1:
-    print("Afshin")
+    def menue():
+        print("1- aficher votre nom ou votre Numero d`etudiant ")
+        print("2- calcule nombre 1 puissan nombre2 ")
+        print("3- le nombre est divisible par 2 ou 3 ?  ")
+        print("4- a quel ane vous seriez 50 ans :  ")
+        print("5- effectuer un operarion de 3 floats, le resultat avec 3 chiffres après la virgule  ")
+        print("6- entrez votre repas , paye et l anne prefere :  ")
+        print("7- sortir  ")
+    menue()   
+    no=int(input("choisir un nombre : " ))
 
-elif(option==2):
-    print("2295368")
+    if no==1:
+        
+        def choisissez_nom_ou_nombre():
+            print("#1. aficher votre nom: ")
+            print("#2. aficher le Numero etudiant: ")
 
-else:
-    print(" votre choix n est pas valid ! ")
-    
-    
-    
-    
-#problem2
+        option=int(input("aficher votre nom(1) ou aficher le Numero etudiant(2) : "))
+        if option==1:
+            print("Afshin Mahjoubi")
 
-def faire_puissance (nombre1,nombre2):
-    return(nombre1**nombre2)
+        elif option==2:
+            print("2295368")
 
-print("#1. entrez nombre1: ")
-print("#2. entrez nombre2: ")
+        else:
+            print(" votre choix n est pas valid ! ")
+       
+        
 
-option1=int(input("Entrez nom(1): "))
-option2=int(input("Entrez nom(2): "))
+    elif no==2:
 
-nombre1=option1
-nombre2=option2
+        def faire_puissance (nombre1,nombre2):
+            return(nombre1**nombre2)
 
-print(faire_puissance(nombre1,nombre2))
+        print("#1. entrez nombre1: ")
+        print("#2. entrez nombre2: ")
 
+        option1=int(input("Entrez nom(1): "))
+        option2=int(input("Entrez nom(2): "))
 
+        nombre1=option1
+        nombre2=option2
 
+        print(faire_puissance(nombre1,nombre2))
 
-#problem3
+        
 
-def diviser_par_3_ou_3():
-  
-    Nombre=int(input("Entrez un nombre : "))
-
-
-    if(Nombre%2==0):
-        print("ce nombre est divisible par 2 :" ,Nombre )
-
-    if(Nombre%3==0):
-        print("ce nombre est divisible par 3 :" ,Nombre )   
-
-    
-diviser_par_3_ou_3()
-
-
+    elif no==3:
 
 
-#problem 4
-
-from re import M
-
-
-def date_de_naissance(Nombre,m):
-    return(Nombre+m)
-
-m=50    
-Nombre=int(input("Entrez votre année de naissance : "))
+        def diviser_par_3_ou_3():
+        
+            Nombre=int(input("Entrez un nombre : "))
 
 
-print("vous etes 50 ans dans cette anne:" , date_de_naissance(Nombre,m))
+            if(Nombre%2==0):
+                print("ce nombre est divisible par 2 :" ,Nombre )
+
+            elif(Nombre%3==0):
+                print("ce nombre est divisible par 3 :" ,Nombre )   
+
+            else:
+                print(Nombre)
 
 
+        diviser_par_3_ou_3()
+        
 
 
+    elif no==4:
 
-#problem5
-
-from unicodedata import decimal
-
-
-def multiplier_et_diviser( p1,p2,p3):
-    return((p1*p2)/p3)
-
-p1=round(7.250)
-p2=round(10.725)
-p3=round(2.256)
-
-print(multiplier_et_diviser(p1,p2,p3)) 
+        from re import M
 
 
+        def date_de_naissance(Nombre,m):
+            return(Nombre+m)
+
+        m=50    
+        Nombre=int(input("Entrez votre année de naissance : "))
 
 
+        print("vous etes 50 ans dans cette anne:" , date_de_naissance(Nombre,m))
+        
 
-#problem6
 
-repas = str(input("entrez votre repas prefere : "))
-pays = str(input("entrez votre pays prefere : "))
-anne = str(input("entrez votre l'année future  : "))
+    elif no==5:
+        from unicodedata import decimal
 
-print(" Vous aurez l'opportunité de manger " , repas , "lorsque vous visiterai " , pays ,  "en" , anne ) 
+
+        def multiplier_et_diviser( p1,p2,p3):
+            return((p1*p2)/p3)
+
+        p1=float(input("entrez premiere numero float :"))
+        p2=float(input("entrez deuxieme numero float :"))
+        p3=float(input("entrez troisieme numero float :"))
+
+        mult=multiplier_et_diviser(p1,p2,p3)
+
+        print(f"{mult:.3f}") 
+        
+
+    elif no==6:
+
+        repas = str(input("entrez votre repas prefere : "))
+        pays = str(input("entrez votre pays prefere : "))
+        anne = str(input("entrez votre l'année future  : "))
+
+        print(" Vous aurez l'opportunité de manger " , repas , "lorsque vous visiterai " , pays ,  "en" , anne ) 
+        
+
+    elif no==7:
+        sortie=True
+
+       
+
+
+menue() 
 
 
